ee35ebd672b22a1659212da07c8fc7354a415761
Anthony <anthonykmbanks@gmail.com>
2021-03-16 16:57:57 -0400

    commit tree e06acbd9d97700773501519df30fad89ef861255

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(thing = `roller-skate`) {
+    return `This Saturday, I want to ${thing}!`
+}
+
+function mondayWork(work = `go to the office`) {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(wrapping = `*`) {
+    return function(phrase = `special`) {
+        let newPhrase = wrapping + phrase + wrapping
+        return `You are ` + (newPhrase) + `!`
+    }
+}
